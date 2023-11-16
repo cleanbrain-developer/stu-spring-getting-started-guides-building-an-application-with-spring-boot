6394b27630fa985ade66ec4b8777918f1c749eeb
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-16 20:22:51 +0900

    first commit

---
+++ src/main/java/com/example/StuSpringGettingStartedGuidesBuildingAnApplicationWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuSpringGettingStartedGuidesBuildingAnApplicationWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuSpringGettingStartedGuidesBuildingAnApplicationWithSpringBootApplication.class, args);
+	}
+
+}
